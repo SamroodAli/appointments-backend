d39af5ea9357663531986005d39068b0674fe123
samroodAli <devsamrood@gmail.com>
2021-11-25 18:22:13 +0530

    Fix: Fix typo for routes for signin and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    post ‘/signin’, to: 'user_token#create'
-    post ‘/signup’, to: 'users#create'
+    post '/signin', to: 'user_token#create'
+    post '/signup', to: 'users#create'
   end
 end
