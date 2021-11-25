f4496134c523b2f91e98ba8e1de9f23fb3c9982c
samroodAli <devsamrood@gmail.com>
2021-11-25 18:03:59 +0530

    User:Configure knock gem for user authentication with tokens

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 module AppointmentsBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 6.1 and config.autoloader = :classic
 
     # Configuration for the application, engines, and railties goes here.
     #
