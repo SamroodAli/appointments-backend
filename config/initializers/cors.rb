36ed81483016e51f9090d4d101c8052e1639ef9c
samroodAli <devsamrood@gmail.com>
2021-11-25 17:49:07 +0530

    Create:Resource User for authentication

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,8 +17,8 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
  allow do
-    origins ‘http://localhost:3000'
-    resource ‘*’,
+    origins 'http://localhost:3000'
+    resource '*',
     headers: :any,
     methods: [:get, :post, :put, :patch, :delete, :options, :head]
  end
