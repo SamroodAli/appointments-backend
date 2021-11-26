5275d7841bc0b5da61b998e3be555d940bfc290e
samroodAli <devsamrood@gmail.com>
2021-11-26 14:37:09 +0530

    Auth:Customise response on auth to respond with jwt and username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::API
-   include Knock::Authenticable
+  include Knock::Authenticable
 end
--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,3 +1,20 @@
 class UserTokenController < Knock::AuthTokenController
-    skip_before_action :verify_authenticity_token, raise: false
+  skip_before_action :verify_authenticity_token, raise: false
+  def create
+    response.set_cookie(
+      :jwt,
+      {
+        value: auth_token.token,
+        expires: 30.minutes.from_now,
+        path: '/api',
+        secure: true,
+        httponly: Rails.env.production?
+      }
+    )
+
+    render json: {
+      jwt: auth_token.token,
+      username: User.find(auth_token.payload[:sub]).username
+    }
+  end
 end
