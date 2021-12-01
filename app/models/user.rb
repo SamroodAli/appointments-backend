9475ac4977194d630578d4483912f44e8ac81163
samroodAli <devsamrood@gmail.com>
2021-12-01 15:14:55 +0530

    Associations:Add associations between teachers, appointments and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :username, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  has_many :appointments
 end
