c187cde6b5b46908d87658bf08f978934c913999
samroodAli <devsamrood@gmail.com>
2021-12-09 19:37:19 +0530

    Cors:Turn off cors since no need for cross domain- both react and rails will be in a single domain

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem 'bcrypt'
 gem 'knock'
-gem 'rack-cors'
+# gem 'rack-cors' 
 
 
 group :development, :test do
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,12 +14,12 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
-# config/initializers/cors.rb
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
- allow do
-    origins 'http://localhost:3000'
-    resource '*',
-    headers: :any,
-    methods: [:get, :post, :put, :patch, :delete, :options, :head]
- end
-end
+# # config/initializers/cors.rb
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#  allow do
+#     origins 'http://localhost:3000'
+#     resource '*',
+#     headers: :any,
+#     methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#  end
+# end
